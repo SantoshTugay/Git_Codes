61dc9800f1363912a0bb2077e6fd8ab30052f67d
SantoshTugay <tugay.santoshkumar@gmail.com>
2024-09-29 00:03:55 +0530

    added sum method

--- src/test/java/com/LoginPage.java
+++ src/test/java/com/LoginPage.java
@@ -3,5 +3,12 @@ package com;
 public class LoginPage {
 public static void main(String[] args) {
 	System.out.println("Hi");
+	sum();
+}
+
+public static int sum() {
+	int res=1+1;
+	System.out.println(res);
+	return res;
 }
 }
